585ae667734814c17c5d92da9c15bf3d3118aaf0
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-16 11:03:43 -0600

    passed the tasks array from the App componenet to the Overview component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
+import uniqid from "uniqid";
 import Overview from './Overview';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+      task: {
+        text: '',
+        id: uniqid()
+      },
+      tasks: [],
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: '', 
+        id: uniqid()
+      },
+    });
+  };
  
   render() {
+    const { task, tasks } = this.state;
+
     return (
       <div>
-        <h1>Howdy!</h1>
-        <Overview />
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor='taskInput'>Enter Task: </label>
+          <input 
+            onChange={this.handleChange}
+            value={task.text}
+            type="text" 
+            id="taskInput">
+          </input>
+          <button type='submit'>Submit</button>
+        </form>
+        <Overview tasks={tasks}/>
       </div>
     )
   }
