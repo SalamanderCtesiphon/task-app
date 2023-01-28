fda1cfe3f0f221e3d6293de24f7d544faf20ba65
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-28 14:48:23 -0600

    copied the template from mdn react tutorial

--- src/App.js
+++ src/App.js
@@ -1,72 +1,98 @@
-import React, { Component } from 'react';
-import uniqid from "uniqid";
-import Overview from './Overview';
-import './App.css';
 
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      task: {
-        text: '',
-        id: uniqid()
-      },
-      tasks: [],
-    };
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      task: {
-        text: e.target.value,
-        id: this.state.task.id,
-      },
-    });
-  };
-
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task: {
-        text: '', 
-        id: uniqid()
-      },
-    });
-  };
-
-  delTask = (id) => {
-    this.setState({
-      tasks: [...this.state.tasks.filter(task => task.id !== id)]
-    })
-  }
- 
-  render() {
-    const { task, tasks } = this.state;
-
-    return (
-      <div className='App'>
-        <div><h1>Task App</h1></div>
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor='taskInput'>Enter Task: </label>
-          <input 
-            onChange={this.handleChange}
-            value={task.text}
-            type="text" 
-            id="taskInput">
-          </input>
-          <button type='submit'>Submit</button>
-        </form>
-        <div className='list'>
-        <Overview tasks={tasks}
-        delTask={this.delTask}/>
-        </div>
+const App = (props) => {
+  return (
+    <div className="todoapp stack-large">
+      <h1>Task App</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="labe_lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input_lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn_primary btn_lg">
+          Add
+        </button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
       </div>
-    )
-  }
-  
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-0" type="checkbox" defaultChecked={true} />
+            <label className="todo-label" htmlFor="todo-0">
+              Eat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-1" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-1">
+              Sleep
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Sleep</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Sleep</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-2" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-2">
+              Repeat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Repeat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Repeat</span>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default App;
+export default App
